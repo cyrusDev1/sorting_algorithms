becdcfb35754a9225f6f1f702f1fc8f1611f8567
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-16 13:01:21 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - j; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
